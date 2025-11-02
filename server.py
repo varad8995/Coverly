3aaa0d811492a60b7139a0bcad835b3d0e753a86
varad <varadbhalsing71@gmail.com>
2025-11-02 13:55:34 +0530

    add a new endpoint to download the image

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from app.routes import upload
+from app.routes import upload,download
 from fastapi.middleware.cors import CORSMiddleware
 import asyncio
 import json
@@ -31,6 +31,7 @@ app.add_middleware(
 )
 
 app.include_router(upload.router, prefix="/api", tags=["Upload"])
+app.include_router(download.router, prefix="/api", tags=["Download"])
 
 # --------------------------
 # Redis (Upstash) Setup
